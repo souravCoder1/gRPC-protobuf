eba747e44dd47e8d48a5d0aa6f7da9bf43d35e48
Coder <abc@cd.com>
2023-09-02 20:40:04 +0530

    Serialization & Deserialization

--- src/main/java/com/sourav/protobuf/App.java
+++ src/main/java/com/sourav/protobuf/App.java
@@ -2,13 +2,39 @@ package com.sourav.protobuf;
 
 import com.sourav.protobuf.models.Person;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class App {
-    public static void main(String[] args) {
-        Person person = Person.newBuilder()
+    public static void main(String[] args) throws IOException {
+        Person person1 = Person.newBuilder()
                 .setId(10)
                 .setName("Sourav")
                 .setAddress("New York")
                 .build();
-        System.out.println(person);
+
+        Person person2 = Person.newBuilder()
+                .setId(10)
+                .setName("Sourav")
+                .setAddress("New York")
+                .build();
+
+        System.out.println(person1);
+        System.out.println(person2);
+
+        System.out.println("Check Equal: "+ person1.equals(person2));
+
+        // Serialization & Deserialization
+
+        Path path = Paths.get("person_byte.txt");
+        Files.write(path, person1.toByteArray());
+        byte[] sezPerson = Files.readAllBytes(path);
+        Person desezPerson = Person.parseFrom(sezPerson) ;
+        System.out.println(Arrays.toString(sezPerson));
+        System.out.println(desezPerson);
     }
 }
