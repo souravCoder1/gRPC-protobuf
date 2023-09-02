2023b362d13ccab2aa2c80f2645515a0ddee0aab
Coder <abc@cd.com>
2023-09-02 23:19:47 +0530

    gRPC vs REST

---
+++ src/main/java/com/sourav/protobuf/PerformanceTest.java
@@ -0,0 +1,57 @@
+package com.sourav.protobuf;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.sourav.protobuf.model.JPerson;
+import com.sourav.protobuf.models.Person;
+
+import java.io.IOException;
+
+public class PerformanceTest {
+    public static void main(String[] args) {
+        JPerson person = new JPerson();
+        person.setId(10);
+        person.setName("Sourav");
+
+        // json
+        Runnable json = () -> {
+            ObjectMapper mapper = new ObjectMapper();
+            byte[] serializedJPerson = new byte[0];
+            try {
+                serializedJPerson = mapper.writeValueAsBytes(person);
+                JPerson deserializedJPerson = mapper.readValue(serializedJPerson, JPerson.class);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+        // protobuf
+        Person serializedPerson = Person.newBuilder()
+                .setName("sam")
+                .setId(10)
+                .build();
+        Runnable proto = () -> {
+            try {
+                byte[] bytes = serializedPerson.toByteArray();
+                Person deserializedPerson = Person.parseFrom(bytes);
+            } catch (InvalidProtocolBufferException e) {
+                e.printStackTrace();
+            }
+        };
+
+        runPerformanceTest(json, "JSON");
+        runPerformanceTest(proto, "PROTO");
+    }
+
+    private static void runPerformanceTest(Runnable runnable, String method) {
+        long time1 = System.currentTimeMillis();
+        long time2 = 0l;
+        for (int i = 0; i < 1_000_000; i++) {
+            runnable.run();
+            time2 = System.currentTimeMillis();
+        }
+        System.out.println(method + " : " + (time2 - time1) + " ms");
+    }
+}
---
+++ src/main/java/com/sourav/protobuf/model/JPerson.java
@@ -0,0 +1,12 @@
+package com.sourav.protobuf.model;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class JPerson {
+    int id;
+    String name;
+}
