9d0b6faf64b5522259ec9a0f84250a725a4b2111
Coder <abc@cd.com>
2023-09-04 09:21:24 +0530

    Map

--- src/main/java/com/sourav/protobuf/App.java
+++ src/main/java/com/sourav/protobuf/App.java
@@ -1,12 +1,14 @@
 package com.sourav.protobuf;
 
-import com.sourav.protobuf.models.Person;
+import com.sourav.protobuf.models.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws IOException {
@@ -22,17 +24,60 @@ public class App {
                 .setAddress("New York")
                 .build();
 
+        Address address = Address.newBuilder()
+                .setPostbox(123)
+                .setStreet("main street")
+                .setCity("Atlanta")
+                .build();
+
+        Car civic = Car.newBuilder()
+                .setMake("Honda")
+                .setModel("Civic")
+                .setYear(2005)
+                .build();
+
+        Car accord = Car.newBuilder()
+                .setMake("Honda")
+                .setModel("Civic")
+                .setYear(2005)
+                .build();
+
+
+        List<Car> cars = new ArrayList<>();
+        cars.add(accord);
+        cars.add(civic);
+
+        User user = User.newBuilder()
+                .setName("Sourav")
+                .setAge(25)
+                .addCar(accord)
+                .addCar(civic)
+                //.addAllCar(cars)
+                .setAddress(address)
+                .build();
+
+        Dealer dealer = Dealer.newBuilder()
+                .putModel(2005, civic)
+                .putModel(2020, accord)
+                .build();
+
+        System.out.println(dealer.getModelCount());
+        System.out.println(dealer.getModelOrThrow(2001));
+        System.out.println(dealer.getModelOrDefault(2001, accord));
+        dealer.getModelMap(); // actual Map
+
         System.out.println(person1);
         System.out.println(person2);
+        System.out.println("User: " + user);
 
-        System.out.println("Check Equal: "+ person1.equals(person2));
+        System.out.println("Check Equal: " + person1.equals(person2));
 
         // Serialization & Deserialization
 
         Path path = Paths.get("person_byte.txt");
         Files.write(path, person1.toByteArray());
         byte[] sezPerson = Files.readAllBytes(path);
-        Person desezPerson = Person.parseFrom(sezPerson) ;
+        Person desezPerson = Person.parseFrom(sezPerson);
         System.out.println(Arrays.toString(sezPerson));
         System.out.println(desezPerson);
     }
